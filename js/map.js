68bf0b102ee2ca172b7b0f1e3285c3e893cb306e
yomar_ec <yomar009@hotmail.com>
2024-01-19 21:48:17 -0500

    Ensayo desde vs code

--- js/map.js
+++ js/map.js
@@ -1,8 +1,12 @@
 function myMap() {
   var mapCanvas = document.getElementById("map");
+  // Coordenadas del área metropolitana de Medellín
+  var medellinCoords = new google.maps.LatLng(6.2442, -75.5812);
+
   var mapOptions = {
-    center: new google.maps.LatLng(51.5, -0.2),
-    zoom: 10
+    center: medellinCoords,
+    zoom: 12 // Ajusta el nivel de zoom según tus necesidades
   };
+
   var map = new google.maps.Map(mapCanvas, mapOptions);
 }
